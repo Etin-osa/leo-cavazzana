f1c6f8582a03cfdd1ee0fdc1588a2b7d87d7de69
Etin <frederickade15@gmail.com>
2021-08-09 07:29:14 +0200

    Adding Leo cavazzana project on github

---
+++ dist/Js/main.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+     speed: 1500,
+     cssMode: false,
+     effect: 'fade',
+     loop: true,
+     autoplay: {
+          delay: 9000,
+          disableOnInteraction: false,
+     }
+})
